9c5d5fabc42b39f618c6cd780aca1760680ce184
piyush dev <ootytour119@gmail.com>
2025-02-11 10:46:59 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Logo from "./pages/Logo/Logo";
 import { ToastContainer } from "react-toastify";
 import { Route, Routes } from "react-router-dom";
 
-const socket = io("https://bluecode-jeds.onrender.com");
+const socket = io("https://bluecode-jeds.onrender.com:10000");
 
 function App() {
   const [user, setUser] = useState(true);
