6dd65decb0469f64acabc72f05b181fe3e751430
piyush dev <ootytour119@gmail.com>
2025-02-11 10:19:52 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Make Vite accessible externally
+    port: process.env.PORT || 5173 // Use Render's provided port, or fall back to 5173
+  }
+});
